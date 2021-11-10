18d846031ffc0430479b2fcc9198d532de85acd7
Aline Patrón <89112117+alinepatron@users.noreply.github.com>
2021-11-10 11:44:25 -0600

    Update index.js

--- EjercicioReact/first-app/src/Componente/index.js
+++ EjercicioReact/first-app/src/Componente/index.js
@@ -2,15 +2,22 @@ import React, { useState, useEffect } from "react";
 
 function Componente() {
     const [info, setInfo] = useState();
+    const [info2, setInfo2] = useState();
 
     const handleAsync = async() => {
         const user = await fetch("https://api.github.com/users/alinepatron");
         const userJson = await user.json();
         setInfo(userJson);
     }
+    const handleAsync2 = async() => {
+        const user = await fetch("https://api.github.com/users/erickgtzh");
+        const userJson = await user.json();
+        setInfo2(userJson);
+    }
 
     useEffect(() => {
         handleAsync();
+        handleAsync2();
     }, []);
 
     return (
@@ -18,6 +25,10 @@ function Componente() {
             <img src={info?.avatar_url}/>
             <p>{info?.name}</p>
             <p>@{info?.login}</p>
+
+            <img style={{borderRadius:20}} src={info2?.avatar_url}/>
+            <p>{info2?.name}</p>
+            <p>@{info2?.login}</p>
         </>
     );
 }
