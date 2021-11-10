09773b2ce335207a6f474d821c268edb5c7de64c
Aline Patrón <89112117+alinepatron@users.noreply.github.com>
2021-11-10 11:30:44 -0600

    Create index.js

---
+++ EjercicioReact/first-app/src/Blog/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Componente from './Components/Componente';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Componente />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
